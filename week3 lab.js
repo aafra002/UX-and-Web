f404e9fedde5479780b49e26c1f49d2781806bc9
aafra002 <aafra002@gold.ac.uk>
2024-10-21 11:02:07 +0100

    first commit

---
+++ week3 lab.js
@@ -0,0 +1,37 @@
+let a = 1;
+let b = a++;
+console.log(b)
+
+let a1 = 1;
+let b1 = a1--;
+console.log(b)
+console.log(a)
+
+let a2 = 1;
+let b2 = ++a2;
+console.log(b)
+
+
+let a3 = 1;
+let b3 = a3++;
+console.log(b)
+console.log(a)
+
+
+let a8 = 1;
+a8 += 2;
+console.log(a)
+
+let a9 =1;
+a9+= 2;
+console.log(a)
+
+let age = 40;
+if(age > 18){
+    console.log("Your an adult");
+}
+
+let age41 = 20;
+if(age41 >= 18){
+    console.log("YOUR AN ADULT");
+} else if (age41 >= 13){"Your a teenager"};
